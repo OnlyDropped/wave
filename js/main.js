eca8d92254271fcf69f8db3564397229cf9b0357
OnlyDropped <asheburenkov@yandex.ru>
2021-03-01 23:09:43 +0300

    added menu hamburger

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 $(function(){
-  var mixer = mixitup('.portfolio__content');
+      $('.menu__btn').on('click', function(){
+      $('.menu__list').toggleClass('menu__list--active')
+  });
+
   $('.slider-inner').slick({
     arrows: false,
     dots: true
   });
+
+
+  var mixer = mixitup('.portfolio__content');
+
 });
   
 
 
-
+$('.menu-burger__header').toggleClass('open-menu');
 
 
 
